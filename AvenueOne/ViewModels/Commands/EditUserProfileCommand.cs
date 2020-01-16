8d0cf656891a9f7c91b862d4af03322862ba08c9
shimakee <deleon.services@outlook.com>
2020-01-16 15:15:28 +0800

    refactor userprofile command

--- AvenueOne/ViewModels/Commands/EditUserProfileCommand.cs
+++ AvenueOne/ViewModels/Commands/EditUserProfileCommand.cs
@@ -52,78 +52,53 @@ namespace AvenueOne.ViewModels.Commands
             {
                 if (ViewModel == null)
                     throw new NullReferenceException("View model must not be null in order to execute command.");
-                if (ViewModel.User == null)
-                    throw new NullReferenceException("No item selected to update.");
-                if (ViewModel.Profile == null)
+                if (ViewModel.User == null || ViewModel.Profile == null)
                     throw new NullReferenceException("No item selected to update.");
 
-                //User user = this.ViewModel.User.User as User;
-                if (ViewModel != null && ViewModel.User != null && ViewModel.Account.IsValidProperty("Username"))
+
+                if (!ViewModel.Account.IsValidProperty("Username"))
+                    throw new ArgumentException("Invalid entry in username.");
+                IUser user = await Task.Run(() => _unitOfWork.Users.GetAsync(ViewModel.User.Id));
+                if (user == null)
+                    throw new NullReferenceException("Account does not exist.");
+
+                // to retain original password
+                string password = user.Password; 
+                user = ViewModel.Account.CopyPropertyValues(user);
+                user.Password = password;
+
+                if (!ViewModel.Profile.IsValid)
+                    throw new ArgumentException("Invalid entry on profile");
+                ViewModel.Profile.CopyPropertyValuesTo(user.Person);
+
+                if (ViewModel.IsPasswordIncluded)
                 {
+                    //get parameters
+                    object[] values = (object[])parameter ?? throw new ArgumentNullException("parameter cannot be null, you need to pass password and password confirmbox");
+                    PasswordBox passwordBox = (PasswordBox)values[0];
+                    PasswordBox passwordConfirmBox = (PasswordBox)values[1];
 
-                    IUser user = await Task.Run(() => _unitOfWork.Users.GetAsync(ViewModel.User.Id));
-                    //User user = await Task.Run(()=> _unitOfWork.Users.Find(u => u.Id == ViewModel.Account.Id).FirstOrDefault());
-                    if (user == null)
-                        throw new NullReferenceException("Account does not exist.");
+                    //get password
+                    ViewModel.Account.Password = passwordBox.Password;
+                    ViewModel.Account.PasswordConfirm = passwordConfirmBox.Password;
 
-                    string password = user.Password;
-                    user = ViewModel.Account.CopyPropertyValues(user);
-                    user.Password = password;
+                    if (!ViewModel.Account.IsValidProperty("Password") || !ViewModel.Account.IsValidProperty("PasswordConfirm"))
+                        throw new ArgumentException("Invalid entry on password or password confirmation does not match.");
 
-                    if (ViewModel.IsPasswordIncluded)
-                    {
-                        //get parameters
-                        object[] values = (object[])parameter ?? throw new ArgumentNullException("parameter cannot be null, you need to pass password and password confirmbox");
-
-                        //get username
-                        PasswordBox passwordBox = (PasswordBox)values[0];
-                        ViewModel.Account.Password = passwordBox.Password;
-                        //get password
-                        PasswordBox passwordConfirmBox = (PasswordBox)values[1];
-                        ViewModel.Account.PasswordConfirm = passwordConfirmBox.Password;
-
-                        if (ViewModel.Account.IsValidProperty("Password") && ViewModel.Account.IsValidProperty("PasswordConfirm"))
-                        {
-                            user.Password = HashService.Hash(ViewModel.Account.Password);
-                            user.PasswordConfirm = user.Password;
-                        }
-                        else
-                        {
-                            _displayService.ErrorDisplay("Invalid entry on password or password confirmation does not match.", "Invalid entry.");
-                            return;
-                        }
-                    }
-                    
-                    
-                    
-
-                    if (!ViewModel.Profile.IsValid)
-                    {
-                        _displayService.ErrorDisplay("Invalid entry on profile", "Invalid entry.");
-                        return;
-                    }
-                    ViewModel.Profile.CopyPropertyValuesTo(user.Person);
-
-                    int n = await Task.Run(() => _unitOfWork.CompleteAsync());
-
-                    if (n == 0)
-                    {
-                        _displayService.MessageDisplay("Could not edit accout.", "Account edit");
-                    }
-                    else
-                    {
-                        _displayService.MessageDisplay($"Edited account: {user.Username}.\nAffected rows: {n}.");
-                    }
+                    user.Password = HashService.Hash(ViewModel.Account.Password);
+                    user.PasswordConfirm = user.Password;
                 }
-                else
-                {
-                    _displayService.ErrorDisplay("Invalid entry on ViewModel, User, or null reference.", "Invalid entry.");
-                }
+
+
+                int n = await Task.Run(() => _unitOfWork.CompleteAsync());
+
+                if (n == 0)
+                    throw new Exception("Could not edit accout.");
+                _displayService.MessageDisplay($"Edited account: {user.Username}.\nAffected rows: {n}.");
             }
             catch(Exception ex)
             {
                 _displayService.ErrorDisplay(ex.Message, "Error");
-                throw;
             }
         }
     }
