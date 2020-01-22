6e488b7d4f7363c45ede671ad4dc534d1166ce57
shimakee <deleon.services@outlook.com>
2020-01-23 01:41:49 +0800

    Created room model and assigned it to plutocontext as a dbset

---
+++ AvenueOne/Core/Models/Room.cs
@@ -0,0 +1,84 @@
+﻿using AvenueOne.Core.Models.Interfaces;
+using AvenueOne.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvenueOne.Core.Models
+{
+    public class Room : BaseObservableModel, IRoom
+    {
+        private string _id;
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private decimal _rate;
+        public decimal Rate
+        {
+            get { return _rate; }
+            set { _rate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private rateType _rateType;
+        public rateType RateType
+        {
+            get { return _rateType; }
+            set { _rateType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _floor;
+        public int Floor
+        {
+            get { return _floor; }
+            set { _floor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _maxOccupants;
+        public int MaxOccupants
+        {
+            get { return _maxOccupants; }
+            set { _maxOccupants = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private RoomType _roomType;
+        public RoomType RoomType
+        {
+            get { return _roomType; }
+            set { _roomType = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Room()
+            : base()
+        {
+            this.Id = GenerateId();
+        }
+    }
+}
