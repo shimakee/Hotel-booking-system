9b70aed76cdc162390255469bda896c43a8c6599
shimakee <deleon.services@outlook.com>
2020-03-05 16:10:48 +0800

    alpha 1.019

--- AvenueOne/Properties/AssemblyInfo.cs
+++ AvenueOne/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AvenueOne")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AvenueOne hotel booking system deveolped Shimakee systems")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Shimakee systems")]
 [assembly: AssemblyProduct("AvenueOne")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
