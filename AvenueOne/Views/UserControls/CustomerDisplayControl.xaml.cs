c40029479d0971a538da297896b6d818f469450b
shimakee <deleon.services@outlook.com>
2020-03-09 16:22:03 +0800

    Created working user control for customer.

---
+++ AvenueOne/Views/UserControls/CustomerDisplayControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AvenueOne.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AvenueOne.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomerDisplayControl.xaml
+    /// </summary>
+    public partial class CustomerDisplayControl : UserControl
+    {
+        public static readonly DependencyProperty PersonProperty =
+           DependencyProperty.Register("Person", typeof(Customer), typeof(CustomerDisplayControl), new PropertyMetadata(null));
+        public Customer Person
+        {
+            get { return (Customer)GetValue(PersonProperty); }
+            set { SetValue(PersonProperty, value); }
+        }
+
+        public CustomerDisplayControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
