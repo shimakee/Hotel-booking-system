1a3b2e87bd0e6ba70c4d42bc2f5e1c1523e74bd2
shimakee <deleon.services@outlook.com>
2020-06-05 17:36:56 +0800

    separated customer form control and customer card control instead of the single customer display control.

---
+++ AvenueOne/Views/UserControls/Customer/CustomerCardControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using AvenueOne.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AvenueOne.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomerCardControl.xaml
+    /// </summary>
+    public partial class CustomerCardControl : UserControl, INotifyPropertyChanged
+    {
+        public CustomerCardControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty CustomerProperty =
+           DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerCardControl), new PropertyMetadata(null));
+        public Customer Customer
+        {
+            get { return (Customer)GetValue(CustomerProperty); }
+            set
+            {
+                SetValue(CustomerProperty, value);
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
---
+++ AvenueOne/Views/UserControls/Customer/CustomerFormControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using AvenueOne.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AvenueOne.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomerFormControl.xaml
+    /// </summary>
+    public partial class CustomerFormControl : UserControl, INotifyPropertyChanged
+    {
+        public CustomerFormControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty CustomerProperty =
+           DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerFormControl), new PropertyMetadata(null));
+        public Customer Customer
+        {
+            get { return (Customer)GetValue(CustomerProperty); }
+            set
+            {
+                SetValue(CustomerProperty, value);
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
