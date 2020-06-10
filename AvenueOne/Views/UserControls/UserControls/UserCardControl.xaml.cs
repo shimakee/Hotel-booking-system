b39cbfca73bf6ee6bc29331839a97f766022cef7
shimakee <deleon.services@outlook.com>
2020-06-10 16:00:25 +0800

    created user card, still ugly but working on it.

--- AvenueOne/Views/UserControls/UserControls/UserCardControl.xaml.cs
+++ AvenueOne/Views/UserControls/UserControls/UserCardControl.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AvenueOne.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,30 @@ namespace AvenueOne.Views.UserControls
     /// <summary>
     /// Interaction logic for UserCardControl.xaml
     /// </summary>
-    public partial class UserCardControl : UserControl
+    public partial class UserCardControl : UserControl, INotifyPropertyChanged
     {
         public UserCardControl()
         {
             InitializeComponent();
+        }
+
+        public static readonly DependencyProperty UserProperty =
+           DependencyProperty.Register("User", typeof(User), typeof(UserCardControl), new PropertyMetadata(null));
+        public User User
+        {
+            get { return (User)GetValue(UserProperty); }
+            set
+            {
+                SetValue(UserProperty, value);
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
         }
     }
 }
