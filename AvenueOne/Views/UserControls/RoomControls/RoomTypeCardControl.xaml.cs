8b9d752cd4b95da820f8c87025cf9ade34f43d73
shimakee <deleon.services@outlook.com>
2020-06-22 17:30:57 +0800

    created roomtypecard control. used it iin admin page with unworking edit delete,  add and clear buttons.

---
+++ AvenueOne/Views/UserControls/RoomControls/RoomTypeCardControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using AvenueOne.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AvenueOne.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for RoomTypeCardControl.xaml
+    /// </summary>
+    public partial class RoomTypeCardControl : UserControl, INotifyPropertyChanged
+    {
+        public RoomTypeCardControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty RoomTypeProperty =
+          DependencyProperty.Register("RoomType", typeof(RoomType), typeof(RoomTypeCardControl), new PropertyMetadata(null));
+        public RoomType RoomType
+        {
+            get { return (RoomType)GetValue(RoomTypeProperty); }
+            set
+            {
+                SetValue(RoomTypeProperty, value);
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
